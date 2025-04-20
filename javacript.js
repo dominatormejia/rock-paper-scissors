d9c5fe8f076cefaf6c42c2d30d8bf152869652a4
Dominic Mejia <206021858+dominatormejia@users.noreply.github.com>
2025-04-20 02:53:55 -0500

    Organized and Completed
    Lots of google and replanning later. All
    algorithms have been cut down and cleaned up.
    I made a for loop at the end. I researched
    a lot on loops for it. Some of the changes were
    my ideas with better planning some were ideas
    I picked up looking at other examples.

--- javacript.js
+++ javacript.js
@@ -1,106 +1,80 @@
-// function to have the computers choice, the number generator
-//  could be more accurate as I believe it could be even, bur
-// sufficient for now.
+// Global Variables
+let humanScore = 0;
+let computerScore = 0;
 
 function getComputerChoice() {
-  randomNumber = Math.floor(Math.random() * 100) + 1;
+  randomNumber = Math.trunc(Math.random() * 3);
 
-  //   console.log(randomNumber);
-
-  if (randomNumber <= 33) {
+  if (randomNumber === 0) {
     return "rock";
-  } else if (randomNumber > 33 && randomNumber <= 66) {
+  } else if (randomNumber === 1) {
     return "scissors";
-  } else if (randomNumber > 66 && randomNumber <= 100) {
+  } else if (randomNumber === 2) {
     return "paper";
   }
 }
 
-// Function gets human choice, currently only works for specifically
-//  "Rock", "Scissors", "Paper". May need to be reworked to be case
-// insensitive. As well as currently returns  if another word it put
-//  or misspelling.
-
 function getHumanChoice() {
-  return prompt("Pick: Rock, Paper, or Scissors", "");
-  //    if (choice === "Rock") {
-  //     return "Rock" ;
-  //    } else if (choice === "Paper") {
-  //     return "Paper" ;
-  //    } else if (choice === "Scissors")
-  //     return "Scissors" ;
+  let lowerCase = prompt("Rock, Paper, or Scissors", "");
+  return lowerCase?.toLowerCase();
 }
 
-// function playGame (){}
+// Reworked and simplified choice functions
 
-let humanScore = 0;
-let computerScore = 0;
-
-// This went through several iterations. I found this method the
-//  easiest to wrap my head around. I know i could have opted for
-// ternary, but it would have broke my brain more. it might not be
-//  the cleanest and lots of if..else, but it currently works lol
-
-function playRound(humanChoice, computerChoice) {
-  humanChoice = humanChoice?.toLowerCase();
+function gameLogic(humanChoice, computerChoice) {
+  let result;
 
   if (humanChoice === computerChoice) {
-    return "tie";
+    result = 0;
   } else if (humanChoice === "rock") {
     if (computerChoice === "scissors") {
-      return "won";
+      result = 2;
     } else if (computerChoice === "paper") {
-      return "lost";
+      result = 1;
     }
   } else if (humanChoice === "paper") {
     if (computerChoice === "rock") {
-      return "won";
+      result = 2;
     } else if (computerChoice === "scissors") {
-      return "lost";
+      result = 1;
     }
   } else if (humanChoice === "scissors") {
     if (computerChoice === "paper") {
-      return "won";
+      result = 2;
     } else if (computerChoice === "rock") {
-      return "lost";
+      result = 1;
     }
   }
-  // console.log(humanChoice);
+
+  if (result === 0) {
+    roundResult = console.log("Wow that's a tie!");
+  } else if (result === 1) {
+    roundResult = console.log("Hmm, you lost this round.");
+    ++computerScore;
+  } else if (result === 2) {
+    roundResult = console.log("You won this round!");
+    ++humanScore;
+  }
+  console.log(humanScore);
+  console.log(computerScore);
 }
 
-const humanChoice = getHumanChoice();
-const computerChoice = getComputerChoice();
+// gameLogic(humanChoice, computerChoice);
 
-// playRound(humanChoice, computerChoice);
+function gameLoop(getHumanChoice, getComputerChoice, gameLogic) {
+  let rounds = 10;
+  for (let i = 0; i < rounds; i++) {
+    const humanChoice = getHumanChoice();
+    const computerChoice = getComputerChoice();
+    gameLogic(humanChoice, computerChoice);
 
-function playGame() {
-  let result = playRound(humanChoice, computerChoice);
-
-  if ((result = "won")) {
-    console.log("Oh wow! You won! Congrats!");
-    humanScore++;
-  } else if ((result = "lost")) {
-    console.log("Dang it, you lost.");
-    computerScore++;
-  } else if ((result = "tie")) {
-    console.log("Oh my, it's a tie!");
-  }
-
-  if (humanScore >= 3) {
-    alert("You win!");
-  } else if (computerScore >= 3) {
-    alert("Computer Wins!");
+    if (humanScore === 3) {
+      console.log("You are the winner!");
+      break;
+    } else if (computerScore === 3) {
+      console.log("The computer is the winner!");
+      break;
+    }
   }
 }
-
-// playGame()
-console.log(humanScore);
-console.log(computerScore);
-
-function game() {
-  for (i = 1; humanScore < 3 && computerScore < 3; i++) {
-    playGame();
-  }
-}
-
-game();
+gameLoop(getHumanChoice, getComputerChoice, gameLogic);
